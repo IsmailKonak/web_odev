a817fb47140e74dd745a0d16f9656098b0b15cb6
İsmail Konak <i_konak@hotmail.com>
2023-05-17 17:03:34 +0300

    Login sayfasıolutşruldu php yazıldı

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+
+$k_adi =  "g221210046@sakarya.edu.tr";
+$sifre =  "g221210046";
+
+echo $k_adi;
+// $email = $_POST["k_adi"];
+// $password = $_POST["sifre"];
+
+// // Check if email and password match expected values
+// if ($email == $k_adi && $password == $sifre) {
+//   // Redirect to the desired page
+//   header("Location: /index.html");
+//   exit();
+// } else {
+//   // Redirect back to the same page with an alert message
+//   header("Location: $_SERVER[PHP_SELF]");
+//   exit();
+// }
+
+?>
