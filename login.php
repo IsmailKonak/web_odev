f90e97eb53e9d907741bdc2da3d6c3082665a537
İsmail Konak <i_konak@hotmail.com>
2023-05-18 14:11:06 +0300

    login page tamamlandı

--- login.php
+++ login.php
@@ -3,17 +3,33 @@
 $k_adi =  "g221210046@sakarya.edu.tr";
 $sifre =  "g221210046";
 
+$home_url = "index.html";
+$login_url = "login.html";
+
 $email = $_POST["k_adi"];
 $password = $_POST["sifre"];
 
-// Check if email and password match expected values
+$message1 = "Giriş Başarılı";
+$message2 = "Giriş Başarısız, Lütfen Tekrar Deneyiniz";
+
 if ($email == $k_adi && $password == $sifre) {
-  // Redirect to the desired page
-  header("Location: /web_odev/index.html");
-  exit();
+  
+  echo '<script>
+            var message = "' . $message1 . '";
+            var title = "Giriş Sistemi";
+            var buttonLabel = "OK";
+            alert(message);
+        </script>';
+  echo '<script>window.location.href = "' . $home_url . '";</script>';
 } else {
-  // Redirect back to the same page with an alert message
-  header("Location: $_SERVER[PHP_SELF]");
+  echo '<script>
+            var message = "' . $message2 . '";
+            var title = "Giriş Sistemi";
+            var buttonLabel = "Tekrar Dene";
+            alert(message);
+        </script>';
+  echo '<script>window.location.href = "' . $login_url . '";</script>';
+
   exit();
 }
 
